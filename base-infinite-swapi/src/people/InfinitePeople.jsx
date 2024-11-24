18c1cbadeafcb539e8e78d5cd15d25efd7ab7b2c
Doug Franklin <doug@dougfranklin.com>
2024-11-24 12:00:47 -0500

    Problem with lack of feedback

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -9,13 +9,25 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError } = useInfiniteQuery({
     queryKey: ["sw-people"],
     queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     getNextPageParam: (lastPage) => {
       return lastPage.next || undefined; // "next" property is part of the api json
     },
   });
+
+  if (isFetching) { return (
+    <div className="loading">Fetching...</div>
+  ) }
+
+  // if (isLoading) { return (
+  //   <div className="loading">Loading...</div>
+  // ) }
+
+  if (isError) { return (
+    <div className="loading">Error: { error.toString() }</div>
+  ) }
 
   return (
     <InfiniteScroll
